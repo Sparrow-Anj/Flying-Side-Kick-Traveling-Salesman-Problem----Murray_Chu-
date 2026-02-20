47a2cf03617301e38979534253e8ac9515ec1e51
Sparrow <wbrqnh9p8z@privaterelay.appleid.com>
2026-02-20 16:41:46 +0530

    Update main.jl

--- main.jl
+++ main.jl
@@ -4,10 +4,10 @@ include("src/plotter.jl")
 
 data = LOAD_DATA()
 
-city = BuildRandomCity(data,15)
-SetTruckInformation(data, city, 75.0)
-SetUvInformation(data, city, 113.0, 50.0, [2,5,6,8,12,7,13,14])
-SetServiceInformation(data, 5.0, 5.0)
+city = BuildRandomCity(data,20)
+SetTruckInformation(data, city, 65.0)
+SetUvInformation(data, city, 113.0, 50.0,[2,5,8,12,13,15,17,19])
+SetServiceInformation(data, 1.25, 1.25)
 
 (model, x_i_j, y_i_j_k)  = CreateModel(data)
 
